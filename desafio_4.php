6f40061cacea4e5b320483a538668ae57a171e1c
Arielton <arieltonsilveira@gmail.com>
2020-12-11 15:48:32 -0300

    tudo pronto

---
+++ desafio_4.php
@@ -0,0 +1,60 @@
+<?php
+
+$textoA = "sskgvlql ssw sswqnpm ssqnsfqb smzbsdn sgwxs sgfwvnlp sntjkxdj snfx snvb szt szrqlptf stsbdhg stxcsk stcpq sxr sxfnmprz sxjrvdls sdxb sdltnhhc sdbd srwx srfknn spbgjrrf sct sfnqj sfqhhc sjshjjch sjkgzl sjnnmwgc sjws sjxs sbt sbjjdfjm svg svxw svdsjppw sht ksgwbn kswptsgn kkk kmzgncxb kmxvq kmfbj kgkhch knvth kwmkgwkt kwzgwftw kqjld ktxzgwr ktvc kxn kxwhlb kxf kdcgsn kdhpqqb kpwnqj kpdpnnpw kpclw kcsl kchr kfs kfgjk kjgn klmtsd kbgcgs kbq kbtqks kbhxmzhs kvnt kvncb kvhk msxh mgf mnkbb mqn mqts mztcqnns mthnhnj mxnr mxdsfnrp mxrsk mddjqt mdlk mrfg mrvbzvf mpdtqt mfwbcsm mfv mjqvsd mlgr mln mlqd mlzpnmtt mllzrh mlvqnc mbzz mbrph mbjj mvhp mhjhktn gslm gkzcbzpb gmq gmzzlnbs gmlrv gmvshcs gglsvb gnng gnxqn gnljq gwn gwbsj gqmvrntq gxvsb gdwgkvdr gdj grf gpcch gcvvj gfttqtm gfhbl gjz gjthzxf gjv gbtfrjt gvksp gvzx gvlbhzr nsq nsrfkps nkz nmkngxz nmzjxj nmdkqxmq nmrwm nmpfzqq ngvzwxt nzktmpqv nzxszltk ntvdsvk nth ndshpdx ndn nrckdgk nps ncdvjpmx nfpgcmn nfcv njvtfjzb nbkltk nbcfhr nvd nvlssn nvb wkxrg wmjvx wgkznw wgzsn wglctjf wnm wnz wntwctwz wqrq wqpl wqbgkd wzv wtdjh wxdv wdxkf wrsqmhzt wrhnw wft wjnhv wlfqhjb wbnt wbz wvslqxb wvjqcwb whsvw qswgsjz qmzrxxc qmj qmlktbw qngkc qngl qndf qwzt qwxsv qwxjh qqd qxnms qdvgtktx qrrzh qcwh qjwsk qjxwxkpl qllgk zstmmd zsdhbx zsrgjpkq zshwhw zkr zkp zkptwqph zmczmpdv zgpnwvt znsfn zqk zqdhszqm zxct zdmpmxnr zdrpwhf zrn zrhjlz zpddnvv zpfwkd zpfl zcg zcnlqkx zcw zcrrdn zcp zjwk zjfltkx zlpzdfxm zbf zvcc zvvjjmw zhtcr zhb tsv tshgrt tkq tkh tgzvcfvd tgclznqb tgfcnbtk tnsghxb tnglwhz tnrh tnpfbts tncwlw twgfcnp tqhrgrqf tzbdfj txc tdt tdbnmh tdbvtcz trwzdmtr trl tpqmhsd tpr tprl tfzkbttk tbtgg tbrtrddg thxjhftl thr xssgwgvd xsmkp xsctp xknpn xgbsczr xnxbwjw xnjnc xwmhht xzmkhxzh xtxjlssr xxtgjl xxd xxr xdc xrwlskd xrzbxmfx xfzmxnkf xjqvb xlbkgs xbdmkc xvmhlsm xvgddk xvq dsmrgfc dsnfz dkhs dgcs dqdjkfjl dzqbtml dtqkwlds dtzhgftb dtrtvx dxgl dxtmwj dxcjc dxfgqq ddrlcp drbq dpqqqx dpqlrg dplb dcxrk dfmgm djqtw dbjvzrg dvmq dvghjq dvzgqv dvzwjpl dvv dhsdgtl dhgwcxb dhqjtk dhvp rkkbkf rmwvbcf rmjv rgnfh rgxxxd rgbx rnvgz rwftl rqnm rqwzhtpb rqzkm rqx rzkqpbq rtrdzhfx rtvxwlkg rxnqj rxhmfqvt rdwdsbmt rdt rdjfmww rrjblhl rrjhn rpstwhmm rpw rprttfzb rppxml rpjcm rcqkphs rcqkjb rccprz rchdxlzk rfwkt rfbw rjj rlglprzg rlvp rlhf rbnzmc rbnts rvbhvfj rhrv pswlw psrpdl pkmd pkhw pkhljrjk pmkslsr pmlb pgrv pwkfjvq pwtwmv pqnj pzlmnvzt ptk pttrlsz pdz prmvt ppgmtp pphhzkqd pcxfvmwh pflrwhw pjxzbbvn pbfp pvgt phclgntg phhpdml csrlg csjh ckn cgds cghftk cndhff cqrxqrv cqjgcpx cqjvdp cqb czqmc czrmz cxktcx cxpwfztb cxjkghv cdstrz ccsrfq cfmm cjkph cjrqsqqq cjc clz cbtcxbg cvqzzk cvtpm cvphrj cvhfr chskxbgs chtgqm chcblrbz chjz fsrgrdn fspsrc fsctbdg fkstjcv fkkpgqfn fkfm fmtxnd fmpfq fgjxrs fwk fwcmrbmg fqnnq fqbqgj fqh ftd fxhlskz fpkfmcdb fpx fphkj ffwcbdp ffqkvh ffjdpqst fjsjh fjghhmvj fjnd fjqztm fjhtrm flq fllfkg fbkf fbqs fbr fbhz fhzpkrjh fhdspm jmztd jmblnxj jntsftz jnckpjh jnj jwcclj jqnmd jqtphpr jqfbv jzsmrn jzgn jzt jtkqrsn jxksrj jrlg jpqn jptzk jczhfhx jcbvwss jfmjkqzm jjqmvnmf jjqxmzxr jbk jbfc jvwp jvxnh jvvf jhtwt lstpk lsrgdth lkgfj lkn lmgnxgv lnb lwzzcft lqtcfm lqhffvsx lzvbsqft lxhslfgv ldkgqmqf ldxl ldpv ldhkqqm lrhzmtj lcpq lcvdzn lfjvrhw llrx lbmcpv lbdmwkjp bssszk bsxwclv bsj bsbdwz bkrkfrxc bmpbzz bgktlm bgnfj bnngkrx bwtmm bwvcrjr bzd btc bdfp brtvcrr bcf bcbnjpwg bjmkqvlv bls bld blb bbmp bbh bvmmgpf bvqztj vstnf vkdkghxh vkhzq vmwrt vmdx vmrbr vgrq vghp vnwm vnd vncx vnbhz vwqmz vzclxxb vzhldn vtvwl vxgjh vxrm vxvl vdfznx vrcnl vrhjfk vpmdn vpn vpwgzg vpjsrbmg vpjtt vcspzs vcm vjs vjkb vjt vjjgwlr vjvpn vlkhr vld vhgcnsg vhzjspk hsqj hkssh hkgb hknz hktlcq hkjmfh hmstjcjt hmtbh hmxdvhb hmccg hmbh hgx hngww hndqtr hnj hwz hqqbljzn hqfppb hzmgnrbp hzmbjx hznv htnzmf htr hxx hxcg hxfc hdqfggx hplqmx hcnk hcdfrmc hfpvdhc hjk hjxlrqz hjxbt hlgnmfwm hbktgrl hbcbvh hbfmtsg hbfpqs hvvltwqh";
+$textoB = "sknppkq smm sgtd snfc sqstkmfc sqz sqtpzh sqlqcts sqbs sqbvtz sxrnffz sxl sxbmm sxbjkl sdswmzhb sfvrqm sjlbx slnkxhh svn shc kkkqltcj kmwtfn kmplb kgmcbxbw kgxsmv knvq kwrgbqgq kwp kwl kqcmtsfc kqhzd ktgpcnl ktxlwnr kdgnh kdzqhw kdjc kdv kpbkbjzj kcwwg kjvw kjvqtclf klnrqp klzs kbbbdc kvcn khxgrbbs khxpm khrpjz khjn khblvds msxl mksrstj mkkqzv mgp mnkgwksn mndzhhrn mnrwkxmf mnlcfm mtqlvbz mtlv mxgsz mxx mdsqbjn mdhlxjlc mpzmh mpvl mcgwdb mcxmv mclbllrx mfjkbchx mjx mlp mljg mlvhjc mbp mblq mhszkzn mhpfb mhcxdksh mhjpksdm gsdvgmb gkswdfk gkp gmsr gmmvhnfj gmgzbq ggxzj gnrxr gwkfkk gqbwc gzksgn gtqf gtd gtjdbsxd gxffj gdmzq gdvcghsj grkklsd grmhcn gpcqwfnm gcsdqnvd gczkchl gcpqszhd gfsxvt gflx gfvzx gjsm gljrhbjm gbncrdn gbpq gblmp gvsgfk gvjpksm ghfsg ghj nsmw nsvps nkp ngg nggj ngqdn nnbgxdv nnvvmr nqqrwlmq nzpnflm nzvdgfdz ntqtr ntz nxm nxfhdd ndc nrknnw ncvhppg nfckbph njrk nlrbbfnz nbpznvl nvxhn nvjfvgr wsmfd wstz wsdgdwm wksdl wkltvsj wgmn wngcvvs wwcrmckx wqp wtzjb wxwc wdxh wrghkrmv wrnzhz wrvkqchh wpkjbtc wpmfpj wprn wpbcggw wphlgkk wcz wfcx wjzqmww wlpdztrq wlpbnvmh wblpgmt wvg whqt whb qkrgtmw qkfbrfks qkjf qmw qmcjhk qgzgn qgfpt qnsz qnqgjcmk qnx qwpx qqhzv qznzjwps qzctb qzcxr qtwpggd qtbr qxqscnn qxfsnpfd qxflc qdf qdbtmf qcmbtpn qfkqbxs qfvq qjkf qjgrd qlqwfqw qbd qbvv qbhzgqll qvmh qhkgsjb qhkqf qhw qhfgjqc zsfcgl zgqrrthv zghwvz zwsptc zwj zwlfrrf zqw zzq zzhtcsch zxzvgkl zxp zdq zdtnv zrnwswv zrvbg zcmsml zjkhh zjz zjfvlm zlxqkpjh zlrtnv zbdpxwb zbffh zvdjvzwf zvvl zvhzh zhtxhnw tspxv tknbv tgxwxq twd tzprqrzd tttgq tdm tcdhdh tfnczfv tftsntvl tfxkc tfxjxl tfrnn tffjh tjxphrbl tlkk tlxzml tvj thvvpw xgrvk xwmvsrwt xwzdtc xwh xqd xqp xqfhxxqm xzglplfm xzpf xxgjkkv xxq xxxdqtqs xdg xpzmm xcsjdmqd xckd xcxrv xbg xbdbhgn xvw xvq xvvk xhnv xhtwncs xhjwdjn dkzwxwb dmmzsd dmrh dmvnt dgqpcnlf dgzzz dgjgscq dnmndfdb dwr dqs dqnz dqqdj dqt dzbcrjdt dtrct dxjvvkc dxhhbfx drll dpxrdvsb dplrnkb dcscpkt dfskpcqg dfp djmb djfxw dlkdbpcc dldx dbnstvs dbt dbdw dbln dbvwzx dvvlf dhdpqs dhfvxzv dhvrbd rsjbksz rkmf rkfxmbb rmzq rgmfrbz rgrgjr rqtkkvmz rzdlqr rzfmgmkg rtjm rxkjpz rxtg rdzfzssf rrk rrmtrhvl rrfl rrblscgh rcsrxbg rcnqb rczmp rcjh rfkflbrl rfx rfjzcbr rjtm rjxwbjck rjpvrvxt rlwzsp rhw psppq pkwsgpkq pkhcqv pmmk pmnjknt pnhtlz pwwcbm pwtmmtbb pwhvzvb pqmq pqq pzk pzft ptm ptw ptpvt ptjs pdr prxwnc prjg prlmmjl ppmbk pcksqscx pcnbsgk pcddvpvq pfwgmt pfjddmh pfh pjl plk plwgkd plwlv pltnmkxp plpv pbrxwg phk phdhf csxcj cktptxt ckcrkn cnxkt cwrl cwbrg cqssd cdrnj cdptv crlf cpqdbzq cpbdt cphwjsf ccrrzss ccjrs cfnbvgc cfczwdq clwktq cld cldzlpbj cljqc cbtx cbpqd cbpc cvklph cvp cvl chwxqqz fsdjq fsb fkxxnlq fkrwxt fkc fmp fmf fgxqxkxv fgczc fgjbrl fwbwx fqgqqwn fqxqlfp fqcgqrpd fzng ftx ftvcl fxwdvkcf fxjp fdtqld fpxmn fcs fczxw fftrzzj ffczq fjs fjngzp fjzgl flpdgpdn fbdcsp fvd fhrxczdt jsmhc jsgzbtp jsxswbs jscwzcs jslzn jgj jncp jwtps jqr jqpf jqvtvl jzms jzwtdnjr jzxzgnqg jzxxmcr jtqc jxcx jddq jdrp jdb jdbpfg jrjnvv jrhw jpm jpgpxmpp jpqhj jchdrrx jfnfb jfjqtthw jfllw jlrpqq jlvmqk jbsfpjvf jbbgxh jbhfv jvbwcvx jvvzhl jhcr jhhxzr lsplljd lsjfjgk lknx lmfbhf lgxz lnxxwts lwkjg lwqtczct lqc ltprfkmr lrtsmb lpw lpxxtxd lpp lctrrb lfsck lfjxrt ljmzdcc lbm lhj lhlgnm lhhp bsc bkkjbv bkhnffcz bmwv bmxxln bmpg bmvl bgtck bnmblz bndnfntl bqgv bqqz bzqnrlhk btnz btdvdfll btpmdgjw bxdpzcmb bxhcq bdsgmtr bdszqpld brpg bcqpt bchnjbss bfdz bfvgkd bjgmkccr bjjkwf bjh blr bbmzjrk bvfg bhx bhjprg vsng vsztcts vmswpj vmgbgs vmxzqgw vgw vgdp vns vwglzdqx vwrjh vwcj vqkrmb vqbl vzk vzkmmb vzkftwnn vzcqxp vtsrjk vtqvvmhl vtxtbw vtjrn vdg vdphfj vdbh vrmlt vrqdrnnk vrtkkk vrvzpn vpztrvcl vpdm vfpwt vfb vjm vjzpcl vljpq vlvwzskt vbdndvc vbvhqmp vvpsnr vvjx hsbcxtr hkgjqk hkc hmm hmphlgk hmlbvs hghvwn hnjgbtcz hwsrt hwmt hwnklbn hxxwp hxppvvdx hdkm hdz hdr hrwltw hrznwg hrplwzjb hpvnp hcc hfmbpn hftwpzwm hlnl hbbfw hvsqth hvsfhns hhdmkrm";
+
+function isNumeroBonito($texto, $alfabeto, $tamanho_num_bonito, $num_divisor){
+    $dados = explode(" ", $texto);
+    $numerosBunitos = [];
+    foreach ($dados as $dado) {
+        $numero = tradutorAlfabetico($alfabeto ,$dado);
+        if ($numero >= $tamanho_num_bonito && 0 == ($numero % $num_divisor)) {
+            $key = array_search($dado, $numerosBunitos);
+            if ($key === false) {
+                array_push($numerosBunitos, $dado);
+            }
+        }
+    }
+    print_r(count($numerosBunitos));
+}
+
+function montarArrayAlfabeto($letra, $letra_traducao){
+    if (count($letra) != count($letra_traducao)) {
+        echo "Array de Letras tem tamanho diferente do letra_traducao";
+        return;
+    }
+    $alfabeto = array();
+    foreach ($letra as $key => $value) {
+        $alfabeto[$value] = $letra_traducao[$key];
+    }
+    return $alfabeto;
+}
+
+function tradutorAlfabetico(&$alfabeto,$palavra){
+    $cont = 0;
+    $soma = 0;
+    $base = 1;
+    while (strlen($palavra) > $cont){
+        foreach (array_keys($alfabeto) as $letra) {
+            if ($letra == $palavra[$cont]) {
+                if ($cont <= 0){
+                    $soma = $alfabeto[$letra];
+                }else {
+                    $base = $base * 20;
+                    $soma = $soma + ($alfabeto[$letra] * $base) ;
+                }
+            }
+        }
+        $cont++;
+    }
+    return $soma;
+}
+
+$alfabeto_numerico = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
+$alfabeto_googlons = ['s', 'k', 'm', 'g', 'n', 'w', 'q', 'z', 't', 'x', 'd', 'r', 'p', 'c', 'f', 'j', 'l', 'b', 'v', 'h'];
+
+$alfabeto = montarArrayAlfabeto($alfabeto_googlons, $alfabeto_numerico);
+
+isNumeroBonito($textoB, $alfabeto, 563131, 5);
+
+// Resposta: 78
